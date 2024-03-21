f59cd66cce87d1c4f149bc133d4e0733c09ad687
nick <nguamanalvarado@iesmordefuentes,com>
2024-03-21 12:20:01 +0200

    mejora

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Http\Controllers\ScraperController;
+use App\Models\Periodico;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Search\PeriodicoController;
+
+
+
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('periodico');
 });
+Route::post('/cons', [PeriodicoController::class, 'buscarPorId'])->name('periodicos');
+
+Route::get('/scraper',[ScraperController::class, 'scrape']);
 
 require __DIR__.'/auth.php';
